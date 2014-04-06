42cfbc1f8bf9e43e2902087a9819a474bc8f1cfd
dfudger <daniellefudger@gmail.com>
2014-04-06 13:26:55 -0400

    Created the bookmarks resource. Added the view for bookmarks/new

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ BookmarkingTool::Application.routes.draw do
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :bookmarks
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root to: 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
